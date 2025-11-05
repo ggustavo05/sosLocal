0b5642649221cee49633fd139f119ae9bd565ea0
ggustavo05 <gustavogoncalves719@gmail.com>
2025-11-05 10:58:54 -0300

    Estilização aprimorada do RiskAreaSection com layout centralizado e botão destacado

--- components/home/RisckAreaSection.tsx
+++ components/home/RisckAreaSection.tsx
@@ -22,3 +22,56 @@ export default function RiskAreaSection() {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff',
+    paddingHorizontal: 20,
+    paddingVertical: 20,
+    marginHorizontal: 20,
+    marginVertical: 10,
+    borderRadius: 15,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.1,
+    shadowRadius: 8,
+    elevation: 5,
+  },
+  content: {
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 16,
+    color: '#333',
+    textAlign: 'center',
+    marginBottom: 20,
+    lineHeight: 22,
+  },
+  button: {
+    backgroundColor: '#8B0000',
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 30,
+    paddingVertical: 15,
+    borderRadius: 10,
+    shadowColor: '#8B0000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
+    elevation: 3,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: '600',
+  },
+  buttonIcon: {
+    marginLeft: 8,
+  },
+});
