c92533e197f585ad5b6a1acbb1e8bfa6edda326c
ggustavo05 <gustavogoncalves719@gmail.com>
2025-11-05 09:18:55 -0300

    Refatora: estilos do CepSection para melhor alinhamento e visual

--- components/home/CepSection.tsx
+++ components/home/CepSection.tsx
@@ -24,3 +24,65 @@ export default function CepSection() {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff',
+    paddingHorizontal: 20,
+    paddingVertical: 20,
+    marginHorizontal: 20,
+    marginVertical: 10,
+    borderRadius: 15,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.1,
+    shadowRadius: 8,
+    elevation: 5,
+  },
+  content: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  textContainer: {
+    flex: 1,
+    marginRight: 15,
+  },
+  text: {
+    fontSize: 16,
+    color: '#333',
+    lineHeight: 22,
+  },
+  highlight: {
+    color: '#D2691E',
+    fontWeight: 'bold',
+  },
+  button: {
+    backgroundColor: '#1B547D',
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    paddingVertical: 12,
+    borderRadius: 10,
+    minWidth: 140,
+    shadowColor: '#1B547D',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
+    elevation: 3,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: '600',
+  },
+  buttonIcon: {
+    marginLeft: 8,
+  },
+});
